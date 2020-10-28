b6b7e6ca21448c6080b4817f3a06647309cc5fc6
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-10-28 13:29:20 -0700

    adds link to heroku backend

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ An application that provides users the ability to practice buying and selling S&
 
 - Results should remain between sessions. Only show the last 10 calculations descending from most recent to oldest. The calculator only needs to implement basic arithmetic operations, although you can add other math functions if you would like to impress. But don't forget to meet the basic requirements of the exercise first!
 
+## Live Demo
+
+Visit https://sezzle-calculator-matthew-sedlacek.netlify.app/
+
 ## Local Installation
 
 1. Fork this repository
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
-export const API_ROOT = "http://localhost:3000";
-export const API_WS_ROOT = "ws://localhost:3000/cable";
+// export const API_ROOT = "http://localhost:3000";
+// export const API_WS_ROOT = "ws://localhost:3000/cable";
+export const API_ROOT = "https://sezzle-calculator-backend.herokuapp.com";
+export const API_WS_ROOT =
+  "ws://https://sezzle-calculator-backend.herokuapp.com/cable";
 export const HEADERS = {
   "Content-Type": "application/json",
   Accept: "application/json",
 };
-
-// export const API_ROOT = "https://powerful-cove-61418.herokuapp.com";
-// export const API_WS_ROOT = "ws://powerful-cove-61418.herokuapp.com/cable";
