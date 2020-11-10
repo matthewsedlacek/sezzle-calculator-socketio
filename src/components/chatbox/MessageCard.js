07e485c474ffd1b864e7d2ca7bee0863435569d5
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-11-10 11:58:02 -0800

    uses moment to update times to local

--- src/components/chatbox/MessageCard.js
+++ src/components/chatbox/MessageCard.js
@@ -4,7 +4,13 @@ import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
+const moment = require("moment");
+
 const MessageCard = (props) => {
+  let dateString = props.timestamp;
+  let dateObj = new Date(dateString);
+  let momentObj = moment(dateObj);
+  let momentString = momentObj.format("MMMM Do YYYY, h:mm:ss a");
   return (
     <ListItem>
       <Grid item xs={12}>
@@ -14,10 +20,7 @@ const MessageCard = (props) => {
               align="left"
               primary={props.messageText}
             ></ListItemText>
-            <ListItemText
-              align="left"
-              secondary={props.timestamp}
-            ></ListItemText>
+            <ListItemText align="left" secondary={momentString}></ListItemText>
             <ListItemText
               align="left"
               secondary={props.username}
@@ -30,10 +33,7 @@ const MessageCard = (props) => {
               align="right"
               primary={props.messageText}
             ></ListItemText>
-            <ListItemText
-              align="right"
-              secondary={props.timestamp}
-            ></ListItemText>
+            <ListItemText align="right" secondary={momentString}></ListItemText>
             <ListItemText
               align="right"
               secondary={props.username}
