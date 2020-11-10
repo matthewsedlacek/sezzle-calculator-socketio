e17c21b56816a7cfdb135efd4cb2e3fd711aed45
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-11-10 00:23:53 -0800

    adds createConsumer to constants file for action cable

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,11 @@
+const actioncable = require("actioncable");
+
 // export const API_ROOT = "http://localhost:3000";
 // export const API_WS_ROOT = "ws://localhost:3000/cable";
 export const API_ROOT = "https://gentle-castle-53687.herokuapp.com";
-export const API_WS_ROOT = "wss://gentle-castle-53687.herokuapp.com/cable";
+export const API_WS_ROOT = actioncable.createConsumer(
+  "wss://gentle-castle-53687.herokuapp.com/cable"
+);
 export const HEADERS = {
   "Content-Type": "application/json",
   Accept: "application/json",
