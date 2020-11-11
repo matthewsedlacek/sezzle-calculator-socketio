e1b805c6636774d121572cb0380aeae0cc929425
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-11-10 19:39:15 -0800

    adds secondary call to action cable subscription to the Cable.js

--- src/components/Cable.js
+++ src/components/Cable.js
@@ -1,10 +1,16 @@
 import React, { Fragment } from "react";
 import { ActionCableConsumer } from "react-actioncable-provider";
 
+const actioncable = require("actioncable");
+const ac = actioncable.createConsumer(
+  "wss://gentle-castle-53687.herokuapp.com/cable"
+);
+
 const Cable = ({ conversations, handleReceivedMessage }) => {
   return (
     <Fragment>
       {conversations.map((conversation) => {
+        ac.subscriptions.create();
         return (
           <ActionCableConsumer
             key={conversation.id}
